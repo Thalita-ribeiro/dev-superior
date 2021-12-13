a1f09d4038a3f4d74f9fd8600e055b3b554e4473
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-12-13 20:31:18 -0300

    Foi deletado os getters e setters que não foram utilizados

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -17,36 +17,9 @@ public class Order {
 
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    public Order() {
-    }
-
     public Order(Date moment, OrderStatus status, Client client) {
         this.moment = moment;
         this.status = status;
-        this.client = client;
-    }
-
-    public Date getMoment() {
-        return moment;
-    }
-
-    public void setMoment(Date moment) {
-        this.moment = moment;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
         this.client = client;
     }
 
