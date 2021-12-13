967795141fd00fff86acdfc4248ee21290248edd
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-12-13 20:11:39 -0300

    Inicializando o projeto

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,28 +1,57 @@
 package application;
 
 import entities.Client;
+import entities.Order;
 import entities.OrderItem;
 import entities.Product;
+import entities.enums.OrderStatus;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        Product product = new Product("TV", 1000.0);
-
-        OrderItem orderItem = new OrderItem(6, 1000.0, product);
-
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-        Client client = new Client("Thalita", "sribeiro.thalita@gmail.com", simpleDateFormat.parse("14/06/2000"));
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(orderItem.toString());
-        System.out.println(client.toString());
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.next();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = simpleDateFormat.parse(scanner.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data:");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(scanner.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("How many items to this order? ");
+        int itensQuantity = scanner.nextInt();
+        for (int i = 1; i <= itensQuantity; i++) {
+            System.out.println("Enter #" + i + " item data:");
+            System.out.print("Product name: ");
+            scanner.nextLine();
+            String productName = scanner.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = scanner.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = scanner.nextInt();
+
+            Product product = new Product(productName, productPrice);
+
+            OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+
+            order.addItem(orderItem);
+        }
 
         scanner.close();
     }
